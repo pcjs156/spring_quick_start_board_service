7f96c67a1caf80a294b74787471ba1263c3696b9
Yubin Park <itqkrdbqls1001@naver.com>
2022-03-14 20:03:45 +0900

    refactor: 5.2.6. 외부 Pointcut 참조하기 (모든 Advice)

--- src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -4,18 +4,13 @@ import com.springbook.biz.user.UserVO;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @Aspect
 public class AfterReturningAdvice {
-    @Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
-    public void getPointcut() {
-    }
-
-    @AfterReturning(pointcut = "getPointcut()", returning = "returnObj")
+    @AfterReturning(pointcut = "PointcutCommon.getPointcut()", returning = "returnObj")
     public void afterLog(JoinPoint jp, Object returnObj) {
         String method = jp.getSignature().getName();
 
