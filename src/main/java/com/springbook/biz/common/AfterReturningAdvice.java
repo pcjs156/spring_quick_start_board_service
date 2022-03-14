03a93cf63c292525e756061f1d6e35658d375fc7
Yubin Park <itqkrdbqls1001@naver.com>
2022-03-14 19:56:37 +0900

    feat: 5.2.2. After Returning 어드바이스

--- src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -2,9 +2,20 @@ package com.springbook.biz.common;
 
 import com.springbook.biz.user.UserVO;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
 
+@Service
+@Aspect
 public class AfterReturningAdvice {
+    @Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
+    public void getPointcut() {
+    }
+
+    @AfterReturning(pointcut = "getPointcut()", returning = "returnObj")
     public void afterLog(JoinPoint jp, Object returnObj) {
         String method = jp.getSignature().getName();
 
